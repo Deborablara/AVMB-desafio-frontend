35f8a66b402ee44a352188122b0db14d1ce9893b
Deborablara <debora.blara19@gmail.com>
2023-09-28 13:47:31 -0300

    criação do card de repositorios

---
+++ src/components/cardRepositorio/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <q-card class="my-card card-container">
+    <q-card-section class="card-section">
+      <div class="card-header">
+        <div class="container-title">
+          <p class="card-title">Repositório 1</p>
+          <p class="create-at">Criado em: 28/09/2023 09:41</p>
+        </div>
+      </div>
+      <div class="button-container">
+        <q-btn 
+          color="primary"
+          label="Ver envelopes" 
+          class="button"
+          icon="mail"
+        />
+      </div>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script lang="ts">
+import {
+  defineComponent,
+} from 'vue';
+
+ 
+
+export default defineComponent({
+  name: 'CardRepositorio',
+});
+</script>
+
+<style lang="scss">
+  @import './style.scss';
+</style>
+
+
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,21 +1,42 @@
 <template>
   <q-page>
-    <div class="col-12 title-container">
+     <div class="col-12 title-container">
       <p class="title">MEUS REPOSITÓRIOS</p>
     </div>
-    <RepositoriosComponent></RepositoriosComponent>
+      <div class="cards-container">
+        <CardRepositorio class="card" />
+        <CardRepositorio class="card" />
+        <CardRepositorio class="card" />
+        <CardRepositorio class="card" />
+        <CardRepositorio class="card" />
+        <CardRepositorio class="card" />
+        <CardRepositorio class="card" />
+        <CardRepositorio class="card" />
+        <CardRepositorio class="card" />
+      </div>
   </q-page>
 </template>
 
+
 <script lang="ts">
 import { defineComponent} from 'vue';
-import RepositoriosComponent from '../../components/repositorios/Repositorios.vue';
+import CardRepositorio from '../../components/cardRepositorio/index.vue';
 
 export default defineComponent({
   name: 'IndexPage',
    components: {
-    RepositoriosComponent, 
-  }
+    CardRepositorio, 
+  },
+  data() {
+    return {
+      cards: [
+        { id: 1 },
+        { id: 2 },
+        { id: 3 },
+        { id: 4 },
+      ],
+    };
+  },
   }
 );
 </script>
